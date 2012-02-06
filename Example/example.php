adc6024e588559bd8ddd8d2f0ad68e3e9ebebc5c
Julien 'ruian' Galenski <galenski.julien@gmail.com>
2012-02-06 23:02:15 +0100

    Give more comment into Example

--- Example/example.php
+++ Example/example.php
@@ -3,11 +3,13 @@ require_once __DIR__ . '/../vendor/.composer/autoload.php';
 
 use Ruian\Benchmark\Bintje;
 
+// Initiate an array with some variables
 $test = array();
 for ($i=0; $i < 50; $i++) { 
     $test[] = uniqid();
 }
 
+// Start the Bench
 $bench = new Bintje();
 $bench->start();
 
@@ -16,17 +18,17 @@ for ($i=0; $i < $length; $i++) {
     echo $test[$i];
 }
 
+// Wait because we need to make the pointer at the beginning 
 $bench->wait();
 reset($test);
 $bench->endwait();
 
 $bench->add();
 
-
-
 foreach ($test as $value) {
     echo $value;
 }
 
+// Stop bench and write it into bintje.log 
 $bench->stop();
 $bench->log(__DIR__);
